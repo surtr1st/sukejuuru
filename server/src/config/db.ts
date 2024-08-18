a04b9bc21361da2ccaab5e9e1672625c28bacfc9
ishi <chitrda112@gmail.com>
2024-08-18 18:56:02 +0700

    chore: change import style of drizzle hook

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,9 +1,9 @@
-import dorm = require('drizzle-orm/postgres-js');
+import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 function useDrizzle() {
     const queryClient = postgres(process.env.SUKEJUURU_CONNECTION_STRING || '');
-    return dorm.drizzle(queryClient);
+    return drizzle(queryClient);
 }
 
 export { useDrizzle };
