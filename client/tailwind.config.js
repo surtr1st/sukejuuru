e82a85b20a69d5e8ef7d8c818041d8c63c3420f7
ishi <chitrda112@gmail.com>
2024-08-27 09:28:48 +0700

    feat(config): configure tailwindcss.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
     content: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#6675FF',
+                secondary: '#819CFF',
+                light: '#F2F5FF',
+                neutral: '#4D4D4D',
+                'neutral-2': '#434A64',
+                dark: '#191B22',
+                danger: '#FF2950',
+                warning: '#FFB629',
+            },
+            fontFamily: {
+                sans: ['Hanken Grotesk', 'sans-serif'],
+                serif: ['Hanken Grotesk', 'serif'],
+            },
+            borderRadius: {
+                '7px': '0.438rem',
+            },
+        },
     },
     plugins: [],
-}
+};
