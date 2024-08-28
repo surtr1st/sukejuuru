7d35ce73e38ae2a5fd798290cfe74fb6f7af6321
ishi <chitrda112@gmail.com>
2024-08-28 16:21:24 +0700

    feat(type): isolate types to external

---
+++ client/src/types/index.ts
@@ -0,0 +1,50 @@
+export type TTask = {
+    id: number;
+    title: string;
+    description: string;
+    minLength: string;
+    maxLength: string;
+    startDate: string;
+    dueDate: string;
+    priority: Partial<TPriority>;
+    criterias: TCriteria[];
+    status: Partial<TStatus>;
+    color: Pick<TColor, 'display'>;
+};
+
+export type TCriteria = {
+    id: number;
+    description: string;
+};
+
+export type TPriority = {
+    id: number;
+    display: string;
+    description: string;
+    createdAt: string;
+};
+
+export type TStatus = {
+    id: number;
+    display: string;
+    description: string;
+    createdAt: string;
+};
+
+export type TColor = {
+    id: number;
+    display: string;
+    createdAt: string;
+};
+
+export type TSize = 'sm' | 'md' | 'lg';
+
+export type TColorVariant =
+    | 'primary'
+    | 'secondary'
+    | 'light'
+    | 'neutral'
+    | 'neutral-2'
+    | 'dark'
+    | 'danger'
+    | 'warning';
