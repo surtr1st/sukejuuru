d3e4bf3418765ca536be8fcf634780916b081cc7
ishi <chitrda112@gmail.com>
2024-09-02 12:54:55 +0700

    feat(component)!: implement RouterView in List

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -2,17 +2,20 @@
     <div class="flex flex-col items-start gap-2 mx-1">
         <h2 class="text-xs font-semibold uppercase text-primary my-1">{{ title }}</h2>
         <ul class="flex flex-col w-full gap-1">
-            <li
-                class="transition ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-start items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
+            <RouterLink
                 v-for="(item, index) in items"
                 :key="index"
-                @click="item.action"
+                :to="item.route"
             >
-                <component :is="item.icon" />
-                <h3 class="font-semibold">
-                    {{ item.label }}
-                </h3>
-            </li>
+                <li
+                    class="transition ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-start items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
+                >
+                    <component :is="item.icon" />
+                    <h3 class="font-semibold">
+                        {{ item.label }}
+                    </h3>
+                </li>
+            </RouterLink>
         </ul>
     </div>
 </template>
