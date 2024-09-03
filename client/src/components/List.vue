3b422e4c92aad11e172d9cdc1f796a3fccc793b3
ishi <chitrda112@gmail.com>
2024-09-03 18:37:58 +0700

    feat(component)!: make List shrinkable

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="flex flex-col items-start gap-2 mx-1">
+    <div
+        v-if="shrink"
+        class="flex flex-col items-center gap-2 mx-1"
+    >
         <h2 class="text-xs font-semibold uppercase text-primary my-1">{{ title }}</h2>
         <ul class="flex flex-col w-full gap-1">
             <template
@@ -7,7 +10,37 @@
                 :key="index"
             >
                 <li
-                    class="transition ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-start items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
+                    class="transition ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-center items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
+                    v-if="item.action"
+                    @click="item.action"
+                >
+                    <component :is="item.icon" />
+                </li>
+                <RouterLink
+                    :to="item.route || ''"
+                    v-else
+                >
+                    <li
+                        class="transition ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-center items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
+                    >
+                        <component :is="item.icon" />
+                    </li>
+                </RouterLink>
+            </template>
+        </ul>
+    </div>
+    <div
+        v-else
+        class="flex flex-col items-start gap-2 mx-1"
+    >
+        <h2 class="text-xs font-semibold uppercase text-primary my-1">{{ title }}</h2>
+        <ul class="flex flex-col w-full gap-1">
+            <template
+                v-for="(item, index) in items"
+                :key="index"
+            >
+                <li
+                    class="transition-all ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-start items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
                     v-if="item.action"
                     @click="item.action"
                 >
@@ -17,11 +50,11 @@
                     </h3>
                 </li>
                 <RouterLink
-                    :to="item.route"
+                    :to="item.route || ''"
                     v-else
                 >
                     <li
-                        class="transition ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-start items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
+                        class="transition-all ease-in-out w-full h-[48px] py-1 px-3 border-l border-primary flex gap-5 justify-start items-center hover:cursor-pointer hover:bg-h-light dark:text-light dark:hover:bg-h-dark"
                     >
                         <component :is="item.icon" />
                         <h3 class="font-semibold">
@@ -40,10 +73,25 @@ import { RouterLink } from 'vue-router';
 
 type TList = {
     title: string;
+    shrink: boolean;
     items: TListItem[];
 };
+
 withDefaults(defineProps<Partial<TList>>(), {
     title: 'No Title',
     items: () => [],
+    shrink: false,
 });
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+</style>
