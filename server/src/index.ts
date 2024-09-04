31774a8966e5a971c37de16c4090b018d8f0e633
ishi <chitrda112@gmail.com>
2024-09-04 18:32:32 +0700

    feat!: discard profile model, test connection fn

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,5 @@
 import { Elysia } from 'elysia';
-import { useDrizzle, profile } from './config';
 
 const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
 
-async function testConnection() {
-    try {
-        const db = useDrizzle();
-        console.log('test');
-        const profiles = await db.select().from(profile);
-        console.log(profiles);
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-testConnection();
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
--- server/src/models/profile.ts
+++
@@ -1,9 +0,0 @@
-import { t } from 'elysia';
-
-const Profile = t.Object({
-    id: t.Integer(),
-    displayName: t.String(),
-    joinedAt: t.Date(),
-});
-
-export { Profile };
