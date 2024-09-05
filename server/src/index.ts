355852b6256792cd85245b3993580291ccbed21e
ishi <chitrda112@gmail.com>
2024-09-05 19:46:31 +0700

    feat: add group path and test node controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { Elysia } from 'elysia';
+import { NodeController } from './controllers';
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+const app = new Elysia().group('/api', (app) => app.use(NodeController)).listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
