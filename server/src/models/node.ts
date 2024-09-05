3d0cfad72ca7c5e53750bc7a953598e6c7ba5d38
ishi <chitrda112@gmail.com>
2024-09-05 19:44:42 +0700

    feat(model)!: create reusable node models

--- server/src/models/node.ts
+++ server/src/models/node.ts
@@ -1,7 +1,14 @@
-import { t } from 'elysia';
+import Elysia, { t } from 'elysia';
 
 export const Node = t.Object({
     id: t.Integer(),
     title: t.String(),
     createdAt: t.Date(),
+});
+
+export const NodeDTO = t.Omit(Node, ['id', 'createdAt']);
+
+export const nodeModels = new Elysia({ name: 'node.model' }).model({
+    'node.m': Node,
+    'node.dto': NodeDTO,
 });
