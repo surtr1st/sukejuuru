baa3740f08887e6714dc04921234fad98b9860e0
ishi <chitrda112@gmail.com>
2024-09-07 13:05:56 +0700

    feat(extensions): add toTitleCase extension method

--- server/src/extensions/index.ts
+++ server/src/extensions/index.ts
@@ -1,3 +1,7 @@
 Array.prototype.empty = function (): boolean {
     return this.length === 0;
 };
+
+String.prototype.toTitleCase = function (): string {
+    return this.replace(/([A-Z])/g, ' $1').replace(/^./, (match: string) => match.toUpperCase());
+};
