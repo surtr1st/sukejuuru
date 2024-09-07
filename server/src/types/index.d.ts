672564a4eb4c794764166d4bcc1a6be26595df89
ishi <chitrda112@gmail.com>
2024-09-07 13:03:17 +0700

    feat(type): add TPenalty, TOmits

--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -3,3 +3,14 @@ declare type TNode = {
     title: string;
     createdAt: Date;
 };
+
+declare type TPenalty = {
+    id: number;
+    title: string;
+    description: string;
+    compensation: string;
+    createdAt: Date;
+    nodeId: number;
+};
+
+declare type TOmits<T, K extends keyof T> = Array<Omit<T, K>>;
