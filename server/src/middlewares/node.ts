364335088e9db416ad171019508747aec391f25d
ishi <chitrda112@gmail.com>
2024-09-07 13:05:07 +0700

    feat(node)!: update new error handlers style for node

--- server/src/middlewares/node.ts
+++ server/src/middlewares/node.ts
@@ -1,23 +1,12 @@
-import { NodeError } from '@/enums';
-import { ValidateError } from '@/errors';
+import { ErrorHandlers } from '@/handlers';
 import { nodeModels } from '@/models';
 import Elysia from 'elysia';
 
 export const NodeMiddlewares = new Elysia({ name: 'node.middlewares' })
     .use(nodeModels)
-    .derive({ as: 'scoped' }, ({ body, set, params }) => ({
-        validatePayload: () => {
-            const { title } = body;
-            if (!title) {
-                set.status = 406;
-                throw new ValidateError(NodeError.INPUT_PAYLOAD('title'));
-            }
-        },
-        validateParams: () => {
-            const { id } = params;
-            if (isNaN(id)) {
-                set.status = 406;
-                throw new ValidateError(NodeError.NOT_A_NUMBER('id'));
-            }
-        },
+    .use(ErrorHandlers)
+    .derive({ as: 'scoped' }, ({ body, validateBodyProps, isIntParams }) => ({
+        validatePayload: () =>
+            validateBodyProps({ requestBody: body as Omit<TNode, 'id'>, requiredKeys: ['title'] }),
+        validateParams: () => isIntParams(),
     }));
