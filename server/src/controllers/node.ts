10770d499f1012784113abe96b5b844b299c71df
ishi <chitrda112@gmail.com>
2024-09-07 11:07:38 +0700

    chore(controller): add local guard, discard redundancy

--- server/src/controllers/node.ts
+++ server/src/controllers/node.ts
@@ -1,12 +1,11 @@
 import Elysia from 'elysia';
 import { NodeService } from '@/services';
 import { nodeModels } from '@/models';
-import { NodeMiddlewares } from '@/middlewares';
 
 export const NodeController = new Elysia({ name: 'node.controller' })
     .use(NodeService)
-    .use(NodeMiddlewares)
     .use(nodeModels)
+    .guard({ as: 'local', body: 'node.dto' })
     .derive({ as: 'scoped' }, ({ nodeService, body, params }) => ({
         retrieveNodes: async () => await nodeService().nodes(),
         createNode: async () =>
