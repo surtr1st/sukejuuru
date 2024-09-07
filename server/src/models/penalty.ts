c3f7c1f5a545ff82d1058c0d82f6c051ea002a56
ishi <chitrda112@gmail.com>
2024-09-07 13:04:31 +0700

    feat(model): make reusable penalty models

--- server/src/models/penalty.ts
+++ server/src/models/penalty.ts
@@ -1,4 +1,4 @@
-import { t } from 'elysia';
+import Elysia, { t } from 'elysia';
 
 export const Penalty = t.Object({
     id: t.Integer(),
@@ -6,4 +6,11 @@ export const Penalty = t.Object({
     description: t.String(),
     compensation: t.String(),
     createdAt: t.Date(),
+});
+
+export const PenaltyDTO = t.Omit(Penalty, ['id', 'createdAt']);
+
+export const penaltyModels = new Elysia().model({
+    'penalty.m': Penalty,
+    'penalty.dto': PenaltyDTO,
 });
