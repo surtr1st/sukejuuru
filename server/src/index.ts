a132d5ae20d878b1b6e1c4a1cf6574c864fd66f2
ishi <chitrda112@gmail.com>
2024-09-07 13:06:38 +0700

    feat!: include PenaltyRouter

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import '@/extensions';
 import { Elysia } from 'elysia';
 import { NodeRouter } from './routes';
+import { PenaltyRouter } from './routes/penalty';
 
-const app = new Elysia().group('/api', (app) => app.use(NodeRouter)).listen(3000);
+const app = new Elysia()
+    .group('/api', (app) => app.use(NodeRouter).use(PenaltyRouter))
+    .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
