8ff31b022153b6138558afa7565484aeb48222d3
ishi <chitrda112@gmail.com>
2024-09-08 11:05:46 +0700

    feat(middleware): update error messages and add validate penalty queries

--- server/src/middlewares/penalty.ts
+++ server/src/middlewares/penalty.ts
@@ -1,4 +1,4 @@
-import { PenaltyError } from '@/enums/penalty';
+import { PayloadError } from '@/enums';
 import { ErrorHandlers } from '@/handlers';
 import { penaltyModels } from '@/models';
 import Elysia from 'elysia';
@@ -7,16 +7,21 @@ export const PenaltyMiddlewares = new Elysia({ name: 'penalty.middlewares' })
     .use(penaltyModels)
     .use(ErrorHandlers)
     .guard({ as: 'local', body: 'penalty.dto' })
-    .derive({ as: 'scoped' }, ({ body, validateBodyProps, isIntParams }) => ({
-        validatePayload: () =>
-            validateBodyProps({
-                requestBody: body as Omit<TPenalty, 'id'>,
-                requiredKeys: ['title', 'description', 'compensation'],
-                responseError: {
-                    title: PenaltyError.INPUT_PAYLOAD('title'),
-                    description: PenaltyError.INPUT_PAYLOAD('description'),
-                    compensation: PenaltyError.INPUT_PAYLOAD('compensation'),
-                },
-            }),
-        validateParams: () => isIntParams(),
-    }));
+    .derive(
+        { as: 'scoped' },
+        ({ body, validateBodyProps, isIntParams, validateQueries, query }) => ({
+            validatePayload: () =>
+                validateBodyProps({
+                    requestBody: body as Omit<TPenalty, 'id'>,
+                    requiredKeys: ['title', 'description', 'compensation'],
+                    responseError: {
+                        title: PayloadError.INVALID('title'),
+                        description: PayloadError.INVALID('description'),
+                        compensation: PayloadError.INVALID('compensation'),
+                    },
+                }),
+            validatePenaltyQueries: () =>
+                validateQueries({ requestQueries: query, requiredKeys: ['nodeId'] }),
+            validateParams: () => isIntParams(),
+        }),
+    );
