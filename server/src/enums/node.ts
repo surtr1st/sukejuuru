47633ddb80bff0d1e0e2398898be3f5c07904b3d
ishi <chitrda112@gmail.com>
2024-09-08 10:20:57 +0700

    feat(enum)!: discard Error enums in node and penalty

--- server/src/enums/node.ts
+++ server/src/enums/node.ts
@@ -1,21 +1,9 @@
-const SERVICE_ERROR_TAG = '[ServiceError]:';
-
 export const NodeRoute = {
     RETRIEVE: '/nodes',
     CREATE: '/node',
     UPDATE: '/nodes/:id',
     DELETE: '/nodes/:id',
     FIND_BY_ID: '/nodes/:id',
-};
-
-export const NodeError = {
-    CREATE: `${SERVICE_ERROR_TAG} Cannot insert new node row!`,
-    UPDATE: `${SERVICE_ERROR_TAG} Cannot update node!`,
-    DELETE: `${SERVICE_ERROR_TAG} Cannot delete node!`,
-    INPUT_PAYLOAD: (key: string) => `${key} is invalid!`,
-    NOT_A_NUMBER: (key: string) => `${key} should be a number!`,
-    NONEXISTENCE: (key: string, value: number | string) =>
-        `${SERVICE_ERROR_TAG} ${key} '${value}' does not exist!`,
 };
 
 export const NodeSuccess = {
--- server/src/enums/penalty.ts
+++ server/src/enums/penalty.ts
@@ -1,20 +1,8 @@
-const SERVICE_ERROR_TAG = '[ServiceError]:';
-
 export const PenaltyRoute = {
     RETRIEVE_BY_NODE: '/penalties',
     CREATE: '/penalty',
     UPDATE: '/penalties/:id',
     DELETE: '/penalties/:id',
-};
-
-export const PenaltyError = {
-    CREATE: `${SERVICE_ERROR_TAG} Cannot insert new penalty row!`,
-    UPDATE: `${SERVICE_ERROR_TAG} Cannot update penalty!`,
-    DELETE: `${SERVICE_ERROR_TAG} Cannot delete penalty!`,
-    INPUT_PAYLOAD: (key: string) => `${key} is invalid!`,
-    NOT_A_NUMBER: (key: string) => `${key} should be a number!`,
-    NONEXISTENCE: (key: string, value: number | string) =>
-        `${SERVICE_ERROR_TAG} ${key} '${value}' does not exist!`,
 };
 
 export const PenaltySuccess = {
