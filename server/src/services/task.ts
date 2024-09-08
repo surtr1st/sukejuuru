5d337f6975531e0462f57ab667466383fa8e8115
ishi <chitrda112@gmail.com>
2024-09-08 10:40:32 +0700

    feat(service)!: complete TaskService

--- server/src/services/task.ts
+++ server/src/services/task.ts
@@ -1,9 +1,10 @@
 import { useDrizzle } from '@/config/db';
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import Elysia, { InternalServerError } from 'elysia';
+import Elysia, { InternalServerError, NotFoundError } from 'elysia';
 import { BaseService } from './base';
 import { task } from '@/config/schema';
 import { eq } from 'drizzle-orm';
+import { InternalError, ServiceSuccess } from '@/enums';
 
 interface ITaskService extends BaseService<TTask> {
     findTaskByNode(nodeId: number): Promise<Array<TTask>>;
@@ -22,13 +23,25 @@ class Service implements ITaskService {
 
     async create(value: Omit<TTask, 'id'>): Promise<string> {
         const result = await this.db.insert(task).values(value).returning();
-        if (result.empty()) throw new InternalServerError();
-        return;
+        if (result.empty()) throw new InternalServerError(InternalError('task').CREATE);
+        return ServiceSuccess.CREATE;
     }
 
-    async update(id: number, value: Omit<TTask, 'id'>): Promise<string> {}
+    async update(id: number, value: Omit<TTask, 'id'>): Promise<string> {
+        const target = await this.db.select().from(task).where(eq(task.id, id));
+        if (target.empty()) throw new NotFoundError(InternalError('task').NONEXISTENCE('id', id));
 
-    async remove(id: number): Promise<string> {}
+        const result = await this.db.update(task).set(value).where(eq(task.id, id)).returning();
+        if (result.empty()) throw new InternalServerError(InternalError('task').UPDATE);
+
+        return ServiceSuccess.UPDATE;
+    }
+
+    async remove(id: number): Promise<string> {
+        const result = await this.db.delete(task).where(eq(task.id, id)).returning();
+        if (result.empty()) throw new InternalServerError(InternalError('task').DELETE);
+        return ServiceSuccess.DELETE;
+    }
 }
 
 export const TaskService = new Elysia({ name: 'task.service' }).decorate(
