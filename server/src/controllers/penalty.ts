73ad067029bc1a4a7671c24e301c33e0c0b905b3
ishi <chitrda112@gmail.com>
2024-09-08 11:04:48 +0700

    chore(controller): update penalty controller name

--- server/src/controllers/penalty.ts
+++ server/src/controllers/penalty.ts
@@ -2,7 +2,7 @@ import Elysia from 'elysia';
 import { PenaltyService } from '@/services';
 import { penaltyModels } from '@/models';
 
-export const PenaltyController = new Elysia({ name: 'node.controller' })
+export const PenaltyController = new Elysia({ name: 'penalty.controller' })
     .use(PenaltyService)
     .use(penaltyModels)
     .guard({ as: 'local', body: 'penalty.dto' })
@@ -10,9 +10,7 @@ export const PenaltyController = new Elysia({ name: 'node.controller' })
         retrievePenaltiesByNode: async () => await penaltyService().findByNode(query.nodeId),
         createPenalty: async () =>
             await penaltyService().create({
-                title: body.title,
-                description: body.description,
-                compensation: body.compensation,
+                ...body,
                 createdAt: new Date(),
             }),
         updatePenalty: async () => await penaltyService().update(params.id, body),
