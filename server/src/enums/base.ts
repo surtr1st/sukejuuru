5f9597b70e68a22fc764763ff296bc851610a6de
ishi <chitrda112@gmail.com>
2024-09-08 10:23:05 +0700

    chore(enum): move NONEXISTENCE to InternalError

--- server/src/enums/base.ts
+++ server/src/enums/base.ts
@@ -5,12 +5,12 @@ export const InternalError = (serviceName: TServiceName) => {
         CREATE: `${SERVICE_ERROR_TAG} Cannot insert new ${serviceName} row!`,
         UPDATE: `${SERVICE_ERROR_TAG} Cannot update ${serviceName}!`,
         DELETE: `${SERVICE_ERROR_TAG} Cannot delete ${serviceName}!`,
+        NONEXISTENCE: (key: string, value: number | string) =>
+            `${SERVICE_ERROR_TAG} ${key} '${value}' does not exist!`,
     };
 };
 
 export const PayloadError = {
     INVALID: (key: string) => `${key} is invalid!`,
     NOT_A_NUMBER: (key: string) => `${key} should be a number!`,
-    NONEXISTENCE: (key: string, value: number | string) =>
-        `${SERVICE_ERROR_TAG} ${key} '${value}' does not exist!`,
 };
