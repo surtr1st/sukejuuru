b8b19aea1dc75ff25e20f0aeb09dedfed39fff27
ishi <chitrda112@gmail.com>
2024-09-08 10:18:34 +0700

    feat(type): add TTask, TServiceName

--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -13,4 +13,18 @@ declare type TPenalty = {
     nodeId: number;
 };
 
+declare type TTask = {
+    id: number;
+    title: string;
+    description: string;
+    minLength: BigInt;
+    maxLength: BigInt;
+    createdAt: Date;
+    startDate: Date;
+    dueDate: Date;
+    color: string;
+};
+
+declare type TServiceName = 'node' | 'penalty' | 'task';
+
 declare type TOmits<T, K extends keyof T> = Array<Omit<T, K>>;
