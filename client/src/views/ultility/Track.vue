9dbefa91ff378b3855941105f8e6b8dbefc885cd
ishi <chitrda112@gmail.com>
2024-09-10 10:42:49 +0700

    feat(view): add Track template

---
+++ client/src/views/ultility/Track.vue
@@ -0,0 +1,28 @@
+<template>
+    <TrackHistory :tracks />
+</template>
+
+<script setup lang="ts">
+import TrackHistory from '@/components/TrackHistory.vue';
+import type { TTrackHistoryItem } from '@/types';
+const tracks: TTrackHistoryItem[] = [
+    {
+        taskTag: { color: 'primary', display: 'Computer Science' },
+        description: 'Bruh',
+        duration: '',
+        localTime: '',
+    },
+    {
+        taskTag: { color: 'danger', display: '3D Modeling' },
+        description: 'Bruh',
+        duration: '',
+        localTime: '',
+    },
+    {
+        taskTag: { color: 'warning', display: 'Anatomy Drawing Prac' },
+        description: 'Bruh',
+        duration: '',
+        localTime: '',
+    },
+];
+</script>
