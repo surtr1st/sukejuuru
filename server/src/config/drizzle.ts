5963a944ae12aaf43813a0009dc07f004fe2f875
ishi <chitrda112@gmail.com>
2024-09-20 10:22:24 +0700

    feat!: add postgres + adjust drizzle kit

--- server/src/config/drizzle.ts
+++ server/src/config/drizzle.ts
@@ -1,6 +1,7 @@
-import { usePostgres } from '@bunarcane/arcane';
+import { useResult } from '@bunarcane/arcane';
 import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
 export function useDrizzle() {
-    return drizzle(usePostgres());
+    return drizzle(postgres(useResult(Bun.env.SUKEJUURU_CONNECTION_STRING)));
 }
