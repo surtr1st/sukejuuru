c8e285deea5cd3dd5737b60c16e8d947bb0cccc8
ishi <chitrda112@gmail.com>
2024-09-28 09:32:19 +0700

    feat(component): add Select

---
+++ client/src/components/Select.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="flex items-start flex-col m-1 gap-2">
+        <label
+            v-show="!noTitle"
+            :for="title"
+            class="text-lg font-semibold dark:text-light"
+            >{{ title }}</label
+        >
+        <select
+            :id="title"
+            :class="{
+                'drop-shadow-lg bg-light text-neutral rounded-7px w-full h-[50px] px-3 border border-neutral-2 focus:border-primary outline-none dark:bg-dark dark:text-light dark:border-neutral-2':
+                    !disabled,
+                'drop-shadow-lg text-neutral rounded-7px w-full h-[50px] px-3 border border-neutral-2 outline-none bg-neutral dark:text-light dark:border-neutral-2':
+                    disabled,
+            }"
+            :disabled="disabled"
+            v-model="model"
+        >
+            <option
+                v-for="item in items"
+                :key="item.id"
+                :value="item.id"
+            >
+                {{ item.display }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script setup lang="ts">
+type TItems = {
+    id: number | string;
+    display: string;
+    description: string;
+};
+type TSelect = {
+    title: string;
+    default: string;
+    noTitle: boolean;
+    items: TItems[];
+    disabled: boolean;
+};
+const props = withDefaults(defineProps<Partial<TSelect>>(), {
+    title: 'No title',
+    default: '',
+    noTitle: false,
+    items: () => [],
+    disabled: false,
+});
+const model = defineModel('option', { type: String });
+console.log(props);
+</script>
