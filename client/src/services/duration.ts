d536708fe02eb6c3780b8fde4df28a4fcc0363da
ishi <chitrda112@gmail.com>
2024-10-06 11:46:32 +0700

    feat!: keep the create duration api

--- client/src/services/duration.ts
+++ client/src/services/duration.ts
@@ -4,23 +4,12 @@ import { useFetch } from '@vueuse/core';
 
 export function useDuration() {
     return {
-        logsFromNode: async (nodeId: number) => {
-            const { data } = await useFetch(`${BASE_URL}/durations?nodeId=${nodeId}`)
-                .get()
-                .json<TDuration[]>();
-            return data.value ?? [];
-        },
         createDuration: async (duration: Partial<Omit<TDuration, 'id'>>) => {
-            const { data } = await useFetch(`${BASE_URL}/duration`).post(duration).text();
-            return data.value;
-        },
-        updateDuration: async (id: number, newData: Partial<TDuration>) => {
-            const { data } = await useFetch(`${BASE_URL}/durations/${id}`).put(newData).text();
-            return data.value;
-        },
-        deleteDuration: async (id: number) => {
-            const { data } = await useFetch(`${BASE_URL}/durations/${id}`).delete().text();
-            return data.value;
+            const { data, error } = await useFetch(`${BASE_URL}/duration`)
+                .post(duration)
+                .json<{ insertedId: number }>();
+            if (error.value !== null) throw new Error(error.value);
+            return data.value!.insertedId;
         },
     };
 }
