a7af97eeac64da1495656c7e58ed2a418d3d927b
ishi <chitrda112@gmail.com>
2024-10-06 11:46:04 +0700

    feat!: add default message for toast

--- client/src/helpers/toast.ts
+++ client/src/helpers/toast.ts
@@ -1,7 +1,7 @@
 import { POSITION, TYPE, useToast } from 'vue-toastification';
 import type { ToastOptions } from 'vue-toastification/dist/types/types';
 
-export function useCustomToast(timeout = 3000) {
+export function useCustomToast(timeout = 3000, defaultMessage = 'Unexpected error has occured!') {
     const toast = useToast();
 
     const options: ToastOptions = {
@@ -11,8 +11,10 @@ export function useCustomToast(timeout = 3000) {
         timeout,
     };
 
-    const onSuccess = (message: string) => toast(message, { type: TYPE.SUCCESS, ...options });
-    const onError = (message: string) => toast(message, { type: TYPE.ERROR, ...options });
+    const onSuccess = (message?: string) =>
+        toast(message || defaultMessage, { type: TYPE.SUCCESS, ...options });
+    const onError = (message?: string) =>
+        toast(message || defaultMessage, { type: TYPE.ERROR, ...options });
 
     return { onSuccess, onError };
 }
