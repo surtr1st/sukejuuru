e23b3765f38e4f50c7337747887d33fccf375f68
Ishi <chitrda112@gmail.com>
2024-10-19 19:39:11 +0700

    feat(router): include new routers

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,2 +1,7 @@
 export * from './node';
 export * from './task';
+export * from './color';
+export * from './duration';
+export * from './priority';
+export * from './status';
+export * from './tracker';
