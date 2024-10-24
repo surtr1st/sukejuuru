c9c35814dbd162e02d9092dabf34ebfe3c534c81
Ishi <chitrda112@gmail.com>
2024-10-24 10:04:21 +0700

    feat!: adjust a lot of things

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `${import.meta.env.VITE_SERVER_URL || 'http://localhost:3000'}/api`;
+export const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
 export * from './node';
 export * from './task';
