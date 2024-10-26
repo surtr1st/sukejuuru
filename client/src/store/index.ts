9e012fa40d354b53a3ca9bae3561668acf5ab592
Ishi <chitrda112@gmail.com>
2024-10-26 07:02:16 +0700

    feat!: add useBoolState

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -10,3 +10,16 @@ export const useState = defineStore('appState', {
         colors: [],
     }),
 });
+
+export const useBoolState = defineStore('boolState', {
+    state: () => ({
+        added: false,
+    }),
+
+    actions: {
+        toggleAdded() {
+            this.added = true;
+            setTimeout(() => (this.added = false), 0);
+        },
+    },
+});
